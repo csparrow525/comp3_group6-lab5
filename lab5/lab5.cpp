3b852cad4c6880b575cbfe134e83898737c0b1c9
Charlie Sparrow <cpsparrow525@gmail.com>
2025-03-01 17:31:38 -0500

    second commit - created complex numbers class

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
+
+class ComplexNumber {
+public:
+    //need constructors, accessors and mutators, and overloaded operators below
+
+private:
+    double real;
+    double imaginary;
+};
 
 int main() {
+    //writing the driver here - Charlie Sparrow
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
