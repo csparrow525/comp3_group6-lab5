4308a9e605e30d137f23d482ed371961ddf54321
highsaole <khangvu424@gmail.com>
2025-03-02 03:20:12 -0500

    Did the first three bullet points: wrote constructors, wrote accessors & mutators, and wrote overloaded << operator.

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -5,6 +5,21 @@ class ComplexNumber {
 public:
     //need constructors, accessors and mutators, and overloaded operators below
 
+    // Khang Vu - Constructors
+    ComplexNumber() : real(0), imaginary(0) {}; // Default constructor
+    ComplexNumber(double newReal, double newI) : real(newReal), imaginary(newI) {}; // Value constructor
+
+    // Khang Vu - Mutators + Accessors
+    double Re(void) { return real; }
+    double Im(void) { return imaginary; }
+
+    void setRe(double newR) { real = newR; }
+    void setIm(double newIm) { imaginary = newIm; }
+
+    // Khang Vu - Overloaded << - Friend non-Member
+    friend ostream& operator<<(ostream& outstream, const ComplexNumber& obj);
+
+
 private:
     double real;
     double imaginary;
@@ -12,7 +27,13 @@ private:
 
 int main() {
     //writing the driver here - Charlie Sparrow
-
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
     return 0;
+}
+
+// FUNCTION DEFINITIONS
+// KV - Overloaded <<
+ostream& operator<<(ostream& outstream, const ComplexNumber& obj) {
+    outstream << obj.real << "+" << obj.imaginary << "i";
+    return outstream;
 }
